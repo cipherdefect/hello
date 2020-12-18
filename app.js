2e1eb971dfe562d3df53af49a53a2f08b6b9143f
Daniel Burget <dburget@clearwateranalytics.com>
2020-12-18 15:36:10 -0700

    test upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var info = [
+  [1,"sdff",2,9,"olidfhj",6],
+  [2,5,1,"jdhgwe",4],
+  ["wyuier",2,2,9,2,"jfghwgfb",5],
+  [4,"bnwc",9]
+]
+sum = YourFunctionNameHere(info)
+console.log(sum)
+
+function YourFunctionNameHere(val) {
+  var answer = 0
+  val.forEach(o => {
+    o.forEach(b => {
+      if (typeof(b) === "number") {
+        answer = answer + b
+      }
+    })
+  })
+  return answer
+}
